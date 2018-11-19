01aff926ff39ab3c71ec413d3efd9f1e3d5f5dc6
Natanael <natanso.s17@hotmail.com>
2018-11-19 11:25:46 -0300

    Classe usuário feita

---
+++ src/trello/Usuarios.java
@@ -0,0 +1,36 @@
+package trello;
+
+public class Usuarios {
+    //Atributos
+    private String nome;
+    private String email;
+    private String senha;
+
+    //gettes e settes
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+}
+
